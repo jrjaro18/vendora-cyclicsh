5bd2239760897f4f0d06d9938b3e55a14e21629d
jrjaro18 <rohanjaiswal1500@gmail.com>
2023-07-01 01:54:16 +0530

    ad

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const jwtVerify = require('../middlewares/verifyjwt');
 
-router.get('/search/:id', productController.searchProduct);
-router.get('/get/:id',productController.getProduct);
+router.post('/search/:id', productController.searchProduct);
+router.post('/get/:id',productController.getProduct);
 router.post('/add/review', jwtVerify ,productController.addReview);
 module.exports = router;
